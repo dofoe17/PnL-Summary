e5be883dcebb82c2c177500ddc023718caabccd2
dofoe17 <155560295+dofoe17@users.noreply.github.com>
2025-07-03 18:06:24 +0100

    Add files via upload
    commit with working output email with summary table included.

--- PNL.py
+++ PNL.py
@@ -18,10 +18,16 @@ dt = datetime.today().date()
 
 day_num = dt.strftime('%d')
 
+
+#Monday
+if dt.today().weekday() == 0:
+    dt1 = (dt - timedelta(days=4)).strftime('%Y-%m-%d')
+else: 
+    dt1 = (dt - timedelta(days=2)).strftime('%Y-%m-%d')
+
 #Subtract day_num from today's date 
 som = dt - timedelta(days=int(day_num) - 1)
 
-dt1 = dt - timedelta(days = 2)
 
 last_night_prices = yf.download(tickers=tickers_list, start=dt1, end=dt)['Close']
 Day_before_last_prices = last_night_prices.head(1)
@@ -46,16 +52,16 @@ prices_df = pd.concat([last_night_prices, Day_before_last_prices, SOM_prices, SO
 
 
 DTD_latest = pd.concat([Day_before_last_prices, last_night_prices])
-DTD_pnl = DTD_latest.pct_change()*100
+DTD_pnl = DTD_latest.pct_change()
 
 MTD_latest = pd.concat([SOM_prices, last_night_prices])
-MTD_pnl = MTD_latest.pct_change()*100
+MTD_pnl = MTD_latest.pct_change()
 
 YTD_latest = pd.concat([SOY_prices, last_night_prices])
-YTD_pnl = YTD_latest.pct_change()*100
+YTD_pnl = YTD_latest.pct_change()
 
 ITD_latest = pd.concat([Inception_prices, last_night_prices])
-ITD_pnl = ITD_latest.pct_change()*100 
+ITD_pnl = ITD_latest.pct_change()
 
 
 #Combine to one df 
@@ -65,30 +71,46 @@ pnl_df = pnl_df.reset_index(drop=True)
 
 
 #New index list data
-new_index = {'Timescale': ['ITD', 'YTD', 'MTD', 'DTD']} 
+new_index = {'': ['ITD', 'YTD', 'MTD', 'DTD']} 
 new_index_df = pd.DataFrame(new_index)
 
 
 
 final_pnl_df = pd.merge(new_index_df, pnl_df, left_index=True, right_index=True)
 #final_pnl_df = new_index_df.join(pnl_df) #Same thing as above
-final_pnl_df = final_pnl_df.set_index('Timescale')
+#final_pnl_df = final_pnl_df.set_index('Timescale')
 print(final_pnl_df)
 
 
-#Format table for Email 
-headers = {'selector':'th.col_headings',
-            'props':'background-color:#000066; color:white;'}
 
-table_style = final_pnl_df.style.set_table_styles([headers])
+#Format df table and send email
+import win32com.client as win32
+
+#Format table
+headers = {'selector': 'th.col_heading',
+           'props': [('background-color', 'darkblue'), 
+                     ('color', 'white')]}
+
+borders = {'selector':'',
+           'props': [('border', '1px solid black')]}
 
 
-import win32com.client as win32 
+
+table_style = final_pnl_df.style.set_table_styles([headers, borders]).format({'AAPL': '{:,.2%}',
+                                                                              'AMZN': '{:,.2%}',
+                                                                              'AXP': '{:,.2%}',
+                                                                              'EQNR': '{:,.2%}', 
+                                                                              'GOOGL': '{:,.2%}',
+                                                                              'KO': '{:,.2%}',
+                                                                              'MSFT': '{:,.2%}',
+                                                                              'NKE': '{:,.2%}',
+                                                                              'SONY': '{:,.2%}'}).background_gradient(axis=None).hide()
+
 outlook = win32.Dispatch('outlook.application')
 mail = outlook.CreateItem(0)
 mail.To = 'darren.ofoe@gmail.com'
-mail.Subject = 'PNL Summary' + str(dt1)
-body = '<html><body><h4> Darren Ofoe - PNL History <h4>' + final_pnl_df.to_html() + '</body></html>'
+mail.Subject = 'PNL Summary ' + str(dt)
+body = '<html><body><h4> Darren Ofoe - PNL History <h4>' + table_style.to_html() + '</body></html>'
 mail.HTMLbody = body
 mail.Display() 
 
