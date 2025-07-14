ef1a326ced9f5e6e4bc392c0c97640f0293c2331
dofoe17 <155560295+dofoe17@users.noreply.github.com>
2025-07-14 17:54:12 +0100

    Add files via upload

--- PNL.py
+++ PNL.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time as t 
 from datetime import datetime, timedelta
+import plotly.graph_objects as go 
 
 #def pnl():
 
@@ -47,7 +48,11 @@ Inception_prices = yf.download(tickers=tickers_list, start='2023-10-13', end='20
 #print(Inception_prices)
 
 #Combine to one df 
-prices_df = pd.concat([last_night_prices, Day_before_last_prices, SOM_prices, SOY_prices, Inception_prices]).sort_values(by='Date')
+prices_df = pd.concat([last_night_prices, 
+                       Day_before_last_prices, 
+                       SOM_prices, 
+                       SOY_prices, 
+                       Inception_prices]).sort_values(by='Date')
 #print(pnl_df)
 
 
@@ -65,7 +70,10 @@ ITD_pnl = ITD_latest.pct_change()
 
 
 #Combine to one df 
-pnl_df = pd.concat([ITD_pnl, YTD_pnl, MTD_pnl, DTD_pnl]).sort_values(by='Date').dropna()
+pnl_df = pd.concat([ITD_pnl, 
+                    YTD_pnl, 
+                    MTD_pnl, 
+                    DTD_pnl]).sort_values(by='Date').dropna()
 pnl_df = pnl_df.reset_index(drop=True) 
 #print(pnl_df)
 
@@ -88,23 +96,23 @@ import win32com.client as win32
 
 #Format table
 headers = {'selector': 'th.col_heading',
-           'props': [('background-color', 'darkblue'), 
-                     ('color', 'white')]}
+        'props': [('background-color', 'darkblue'), 
+                    ('color', 'white')]}
 
 borders = {'selector':'',
-           'props': [('border', '1px solid black')]}
+        'props': [('border', '1px solid black')]}
 
 
 
 table_style = final_pnl_df.style.set_table_styles([headers, borders]).format({'AAPL': '{:,.2%}',
-                                                                              'AMZN': '{:,.2%}',
-                                                                              'AXP': '{:,.2%}',
-                                                                              'EQNR': '{:,.2%}', 
-                                                                              'GOOGL': '{:,.2%}',
-                                                                              'KO': '{:,.2%}',
-                                                                              'MSFT': '{:,.2%}',
-                                                                              'NKE': '{:,.2%}',
-                                                                              'SONY': '{:,.2%}'}).background_gradient(axis=None).hide()
+                                                                            'AMZN': '{:,.2%}',
+                                                                            'AXP': '{:,.2%}',
+                                                                            'EQNR': '{:,.2%}', 
+                                                                            'GOOGL': '{:,.2%}',
+                                                                            'KO': '{:,.2%}',
+                                                                            'MSFT': '{:,.2%}',
+                                                                            'NKE': '{:,.2%}',
+                                                                            'SONY': '{:,.2%}'}).background_gradient(axis=None).hide()
 
 outlook = win32.Dispatch('outlook.application')
 mail = outlook.CreateItem(0)
@@ -114,6 +122,23 @@ body = '<html><body><h4> Darren Ofoe - PNL History <h4>' + table_style.to_html()
 mail.HTMLbody = body
 mail.Display() 
 
+
+#Create Visualisation of top performing stock AXP
+ticker = 'AXP'
+vis_df = yf.download(ticker, start='2023-10-13', end=dt1, multi_level_index=False)
+fig = go.Figure(data=[go.Candlestick(x=vis_df.index,
+                                     open=vis_df['Open'],
+                                     high=vis_df['High'],
+                                     low=vis_df['Low'],
+                                     close=vis_df['Close'])])
+
+fig.update_layout(
+    title = f'{ticker} Stock Price',
+    xaxis_title = 'Date',
+    yaxis_title = 'Price (USD)' 
+    #xaxis_rangeslider_visible = False  
+)
+fig.show() 
 
 
 #while(True):
